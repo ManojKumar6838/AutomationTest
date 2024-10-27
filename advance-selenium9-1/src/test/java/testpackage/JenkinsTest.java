00c750838cbc0f91c67a2a836b01f46495b8bfa7
manug <manug@Manoj_Kumar_M.mshome.net>
2024-10-27 13:16:09 +0530

    Added Jenkins Script

---
+++ advance-selenium9-1/src/test/java/testpackage/JenkinsTest.java
@@ -0,0 +1,13 @@
+package testpackage;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class JenkinsTest {
+	
+	@Test
+	public void jenkinsTest() {
+		Reporter.log("Jenkins", true);
+	}
+
+}
